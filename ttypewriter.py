13eebd02789676e67c06a0fc3ee401686d3009fd
Russell Smith <qqrsmith@gmail.com>
2013-04-27 16:56:19 -0400

    Use optparse and add debug_raw option

--- ttypewriter.py
+++ ttypewriter.py
@@ -2,12 +2,13 @@
 import time
 import adc_spi
 import logging
+from optparse import OptionParser
 
 def handle_read(counts):
     if counts < 5:
-	return
+        return
 
-def main():
+def setup():
     logging.getLogger().setLevel(logging.DEBUG)
 
     # Raspberry PI expansion bus GPIO pins to use for SPI bus
@@ -18,21 +19,32 @@ def main():
 
     adc = adc_spi.ADC_SPI(SPICLK, SPIMOSI, SPIMISO, SPICS)
     adc.initadc()
+    return adc
 
+def debug_raw_callback(option, opt, value, parser, adc):
+    debug_raw(adc)
+
+def debug_raw(adc):
     ADC_CHANNEL = 0;                # adc channel to read
-    SENSOR_READ_INTERVAL = 0.01	    # read interval in seconds 
-				    # max speed is ~ 300 sps
-
+    SENSOR_READ_INTERVAL = 0.1      # read interval in seconds 
+                                    # max speed is ~ 300 sps
     start_time = 0
     while True:
         counts = adc.readadc(ADC_CHANNEL)
-	handle_read(counts)
-	dt = time.time() - start_time
-	start_time = time.time()
-
-	logging.debug("counts: %d dt=%f" % (counts, dt))
-
+        handle_read(counts)
+        dt = time.time() - start_time
+        start_time = time.time()
+        logging.debug("counts: %d dt=%f" % (counts, dt))
         time.sleep(SENSOR_READ_INTERVAL)
+
+def main():
+    adc = setup()
+
+    parser = OptionParser()
+    parser.add_option("-d", "--debugraw", action="callback", 
+                    callback=callback_test, callback_args=(adc,),
+                    help="print raw adc values")
+    (options, args) = parser.parse_args()
 
 
 if __name__ == "__main__":
